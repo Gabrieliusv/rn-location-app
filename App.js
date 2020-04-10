d367eeb7ce1d04b8586d8998e17fa678296df636
Gabrieliusv <gabrielius.svi@gmail.com>
2020-04-10 14:28:47 +0300

    screens and navigation setup

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+
+import PlacesNavigator from "./navigation/PlacesNavigator";
 
 export default function App() {
-  return (
-    <View>
-      
-    </View>
-  );
+  return <PlacesNavigator />;
 }
