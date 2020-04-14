0a8582d8bd170fb83234e23d1f88da8f14c36e21
Gabrieliusv <gabrielius.svi@gmail.com>
2020-04-14 20:56:37 +0300

    Added SQLite as DB

--- App.js
+++ App.js
@@ -3,6 +3,16 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 
 import PlacesNavigator from "./navigation/PlacesNavigator";
+import { init } from "./db/db";
+
+init()
+  .then(() => {
+    console.log("Initialized database");
+  })
+  .catch((err) => {
+    console.log("Initializing db failed.");
+    console.log(err);
+  });
 
 export default function App() {
   return (
--- store/actions/placesAction.js
+++ store/actions/placesAction.js
@@ -1,5 +1,20 @@
+import * as FileSystem from "expo-file-system";
+
 import { ADD_PLACE } from "./types";
 
-export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: image } };
+export const addPlace = (title, image) => async (dispatch) => {
+  const fileName = image.split("/").pop();
+  const newPath = FileSystem.documentDirectory + fileName;
+
+  try {
+    FileSystem.moveAsync({
+      from: image,
+      to: newPath,
+    });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
 };
