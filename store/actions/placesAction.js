9e7378cb0460a2bf8763170b6bb22552ee9c2f78
Gabrieliusv <gabrielius.svi@gmail.com>
2020-04-14 22:12:30 +0300

    Inserting and feching places from DB

--- store/actions/placesAction.js
+++ store/actions/placesAction.js
@@ -1,6 +1,7 @@
 import * as FileSystem from "expo-file-system";
 
-import { ADD_PLACE } from "./types";
+import { insertPlace, fetchPlaces } from "../../db/db";
+import { ADD_PLACE, SET_PLACES } from "./types";
 
 export const addPlace = (title, image) => async (dispatch) => {
   const fileName = image.split("/").pop();
@@ -11,10 +12,31 @@ export const addPlace = (title, image) => async (dispatch) => {
       from: image,
       to: newPath,
     });
+    const dbResult = await insertPlace(
+      title,
+      newPath,
+      "Dummy address",
+      15.6,
+      12.3
+    );
+    console.log(dbResult);
+    dispatch({
+      type: ADD_PLACE,
+      placeData: { id: dbResult.insertId, title: title, image: newPath },
+    });
   } catch (err) {
     console.log(err);
     throw err;
   }
+};
 
-  dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+export const loadPlaces = () => async (dispatch) => {
+  try {
+    const dbResult = await fetchPlaces();
+    console.log(dbResult);
+
+    dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+  } catch (err) {
+    throw err;
+  }
 };
